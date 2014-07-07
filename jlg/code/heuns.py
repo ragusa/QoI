caef1eba7b9dc9e6393afa640e3a55670897070e
jlgeer <jlgeer@tamu.edu>
2014-07-06 22:25:27 -0500

    modified:   heuns.py

--- jlg/code/heuns.py
+++ jlg/code/heuns.py
@@ -12,10 +12,10 @@ Solve for int(ru) from 0 to T, using Heun's method
 # -du*/dt = r
 
 
-r = lambda t, u: t if t < 0.5 else 0 # negative derivative of u*
-q = lambda t, u: 1 # derivative function of u
+r = lambda t, u: u**2 # negative derivative of u*
+q = lambda t, u: t**2 # derivative function of u
 
-iters = 100 # Number of steps to take
+iters = 10000 # Number of steps to take
 t = lambda i: i/(iters-1) if i >= 0 else (iters - i)/(iters - 1) # Convert an step number to a time between 0 and 1
 h = t(1)-t(0) # Step Width
 
@@ -33,8 +33,7 @@ for i in range(1,iters):
 	u[i] = u[i-1] + h/2 * (q(t(i-1),u[i-1])+q(t(i), u_tilde))
 # Solve for u*
 for i in range(iters-2, -1, -1):
-	us_tilde = us[i+1] + - h*r(t(i+1), u[i+1])
-	us[i] =  us[i+1] - h/2 * (-r(t(i+1),u[i+1]) - r(t(i), u_tilde))
+	us[i] =  us[i+1] + h/2 * (r(t(i+1),u[i+1]) + r(t(i), u[i]))
 ri = np.array([r(t(i), u[i]) for i in range(iters)])
 qi = np.array([q(t(i),u[i]) for i in range(iters)])
 # Solve forward QoI

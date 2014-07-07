1d489e135ee432cac667411ded4c70f8300727e3
jlgeer <jlgeer@tamu.edu>
2014-07-06 21:44:47 -0500

    new file:   heuns.py

---
+++ jlg/code/heuns.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+'''
+Solve for int(ru) from 0 to T, using Heun's method 
+'''
+
+# QoI = int(r·u) = int(q·u*) - u·u*|_0^T
+
+#  du/dt = q
+# -du*/dt = r
+
+
+r = lambda t, u: t if t < 0.5 else 0 # negative derivative of u*
+f = lambda t, u: 1 # derivative function of u
+
+iters = 100 # Number of steps to take
+t = lambda i: i/(iters-1) if i >= 0 else (iters - i)/(iters - 1) # Convert an step number to a time between 0 and 1
+h = t(1)-t(0) # Step Width
+
+u0 = 0
+u = np.array([None for x in range(iters)])
+u[0] = u0
+
+usN = 0
+us = np.array([None for x in range(iters)])
+us[-1] = usN
+
+# Solve for u
+for i in range(1,iters):
+	u_tilde = u[i-1] + h*f(t(i-1),u[i-1])
+	u[i] = u[i-1] + h/2 * (f(t(i-1),u[i-1])+f(t(i), u_tilde))
+# Solve for u*
+for i in range(iters-2, -1, -1):
+	us_tilde = us[i+1] + - h*r(t(i+1), u[i+1])
+	us[i] =  us[i+1] - h/2 * (-r(t(i+1),u[i+1]) - r(t(i), u_tilde))
+ri = np.array([r(t(i), u[i]) for i in range(iters)])
+fi = np.array([f(t(i),u[i]) for i in range(iters)])
+# Solve forward QoI
+qoif = sum(h * u * ri)
+# Solve adjoint QoI
+qoia = sum(h * us * fi) - (fi[-1]*usN - ri[0]*u0)
+# Print Results
+print("QoI Forward: ", qoif)
+print("QoI Adjoint: ", qoia)
+print("Absolute Difference: ", qoif - qoia)

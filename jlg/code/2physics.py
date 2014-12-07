de67adcdad48b70b1e3f28a6d54be232d9e085b8
jlgeer@tamu.edu <jlgeer@tamu.edu>
2014-12-06 20:10:40 -0600

    modified:   2physics.py

--- jlg/code/2physics.py
+++ jlg/code/2physics.py
@@ -188,122 +188,8 @@ class FEM_SYS:
 		u_for = np.empty((self.T,(self.N+1)**2))
 		u_for[0,:] = u0
 		for t in range(1, self.T+1):
-			u_for[t,:] = forward_time_step(u_for[t-1,:], (t-1)/T)
+			u_for[t,:] = self.newton_solve(u_for[t-1,:],u_for[t-1,:],t)#forward_time_step(u_for[t-1,:], (t-1)/T)
 		return u_for
-	def forward_time_step(self, u_cur, t_cur):
-		"""
-		Takes a single (crank nicolson) time step forward
-		Returns the flux at the next time step.
-		"""
-		raise NotImplementedError('Function not implemented')
-	def newton_jacobian(self, u_cur, u_old, t):
-		"""
-		Calculate the jacobian for the FEM_STATE
-		Takes:
-		u_k, current guess
-		u_cur, flux at time t_cur
-		t_cur, current time
-		R0, initial residual normal
-		"""
-		if u_old is None:
-			u_old = u_cur
-		JAC = spar.dok_matrix(((self.N+1)*2, (self.N+1)*2))
-		# Mass + Stiffness for photon part (Top Left)
-		
-		# Mass + Stiffness for thermal part (Bottom Right)
-		
-		# Mass (Top Right)
-		
-		# (Bottom Left)
-		R0 = self.newton_residual(u_cur, u_old, t)
-		ep = 1E-8
-		for i in range((self.N+1)*2):
-			epsilon = np.zeros((self.N+1)*2)
-			epsilon[i] = ep
-			R1 = self.newton_residual(u_cur+epsilon, u_old, t)
-			D = (R1-R0)/ep
-			for j in range((self.N+1)*2):
-				if D[j] != 0:
-					JAC[j,i] = D[j] 
-		return JAC
-	def newton_residual_half(self, u_cur, u_old, t, d_sys, part):
-		offset = (self.N+1)*part
-		F = np.zeros(self.N+1)
-		
-		# M*u_cur
-		# Previous time step for time dependence.
-		cur_ph_A = self.ph.mass_matrix
-		cur_th_A = self.th.mass_matrix
-		cur_Mu = np.zeros((N+1)*2)
-		cur_Mu[:len(u_cur)//2] = cur_ph_A.dot(u_cur[:len(u_cur)//2])
-		cur_Mu[len(u_cur)//2:] = cur_th_A.dot(u_cur[len(u_cur)//2:])
-		# M*u_old
-		# Previous time step for time dependence.
-		old_ph_A = self.ph.mass_matrix# + self.dt/2 * self.ph.stiffness_matrix(u_old,t - self.dt)
-		old_th_A = self.th.mass_matrix# + self.dt/2 * self.th.stiffness_matrix(u_old,t - self.dt)
-		old_Mu = np.zeros((N+1)*2)
-		old_Mu[:len(u_old)//2] = old_ph_A.dot(u_old[:len(u_old)//2])
-		old_Mu[len(u_old)//2:] = old_th_A.dot(u_old[len(u_old)//2:])
-		for k in range(self.N):
-			x0 = self.x[k]
-			x1 = self.x[k+1]
-			jacob = (x1-x0)/2
-			# Quadrature points mapped to the element
-			x = (x1+x0)/2 + self.xq*jacob
-			# u and du/dx values at quadrature points
-			cur_ph_uq = np.dot(self.b,    u_cur[k:k+self.qorder])
-			cur_th_uq = np.dot(self.b,    u_cur[k+self.N+1:k+self.N+1+self.qorder])
-			old_ph_uq = np.dot(self.b,    u_old[k:k+self.qorder])
-			old_th_uq = np.dot(self.b,    u_old[k+self.N+1:k+self.N+1+self.qorder])
-			
-			cur_uq    = np.dot(self.b,    u_cur[k + offset:k + offset + self.qorder])
-			cur_dudxq = np.dot(self.dbdx, u_cur[k + offset:k + offset + self.qorder])
-			old_uq    = np.dot(self.b,    u_old[k + offset:k + offset + self.qorder])
-			old_dudxq = np.dot(self.dbdx, u_old[k + offset:k + offset + self.qorder])
-			# k(x,t,u) and dk(x,t,u)/du values at quadrature points
-			cur_kq  = d_sys.k(x, t,   cur_ph_uq, cur_th_uq)/jacob
-			old_kq  = d_sys.k(x, t-1, old_ph_uq, old_th_uq)/jacob
-			cur_Dkq = d_sys.Dk(x, t,   cur_ph_uq, cur_th_uq)/jacob
-			old_Dkq = d_sys.Dk(x, t-1, old_ph_uq, old_th_uq)/jacob
-			# sigma(x,t,u) at quadrature points
-			cur_sigq = d_sys.sig(x, t,   cur_ph_uq, cur_th_uq)*jacob
-			old_sigq = d_sys.sig(x, t-1, old_ph_uq, old_th_uq)*jacob
-			# Q(x,t,u) at the quadrature points
-			cur_Qq = d_sys.Q(x, t  , cur_ph_uq, cur_th_uq)*jacob
-			old_Qq = d_sys.Q(x, t-1, old_ph_uq, old_th_uq)*jacob
-			
-			# Mass Matrix Terms
-			cur_Muq = np.dot(self.b, cur_Mu[k + offset:k + offset + self.qorder])
-			old_Muq = np.dot(self.b, old_Mu[k + offset:k + offset + self.qorder])
-
-			local = np.zeros(self.qorder)
-			for j in range(self.qorder):
-				cur_A_term	 = sum(cur_uq*cur_sigq*self.wq*self.b[:,j])
-				cur_A_term	-= np.dot(cur_kq*self.wq*self.dbdx[:,j], cur_dudxq)
-				
-				old_A_term	 = sum(old_uq*old_sigq*self.wq*self.b[:,j])
-				old_A_term	-= np.dot(old_kq*self.wq*self.dbdx[:,j], old_dudxq)
-				
-				cur_Q_term	 = np.dot(cur_Qq*self.wq, self.b[:,j])
-				old_Q_term	 = np.dot(old_Qq*self.wq, self.b[:,j])
-				local[j] = -(cur_A_term+old_A_term)*self.dt/2 - (cur_Q_term+old_Q_term)*self.dt/2
-				local[j] += np.dot(cur_Muq*self.wq,self.b[:,j]) # Time Term  M unew
-				local[j] -= np.dot(old_Muq*self.wq,self.b[:,j]) # Time Term -M uold
-			F[k:k+self.qorder] += local
-		# Apply Boundary Conditions
-		if d_sys.BC.ltype == 0: # Neumann
-			F[0] -= d_sys.BC.left(t)
-		elif d_sys.BC.ltype  == 1: # Robin
-			F[0] += 1/2 * u_cur[offset] - 2*d_sys.BC.left(t)
-		elif d_sys.BC.ltype  == 2: # Dirichlet
-			F[0] = u_cur[offset] - d_sys.BC.left(t)
-		if d_sys.BC.rtype == 0: # Neumann
-			F[-1] -= d_sys.BC.right(t)
-		elif d_sys.BC.rtype == 1: # Robin
-			F[-1] += 1/2 * u_cur[N+offset] - 2*d_sys.BC.right(t)
-		elif d_sys.BC.rtype == 2: # Dirichlet
-			F[-1] = u_cur[N+offset] - d_sys.BC.right(t)
-		return F
 	def newton_residual(self, u_cur, u_old, t):
 		"""
 		Calculate the residual
@@ -311,36 +197,26 @@ class FEM_SYS:
 		u_cur, current guess
 		u_old, solution of previous time step
 		t, current time
-		F0, normal of initial residual 
+		Returns
+		F the residual vector
 		"""
 		if u_old is None:
 			u_old = np.copy(u_cur)
 		F = np.zeros((self.N+1)*2)
-		# Each half of the residual
-		F[0:self.N+1] = self.ph.residual(u_cur, u_old, t, self.dt)#self.newton_residual_half(u_cur, u_old, t, self.ph, 0)
-		F[self.N+1: ] = self.th.residual(u_cur, u_old, t, self.dt)#self.newton_residual_half(u_cur, u_old, t, self.th, 1)
+		F[0:self.N+1] = self.ph.residual(u_cur, u_old, t, self.dt)
+		F[self.N+1: ] = self.th.residual(u_cur, u_old, t, self.dt)
 		return F
-	def newton_residual_norm(self, u_cur, u_old, t, normed=1):
-		if u_old is None:
-			u_old = np.copy(u_cur)
-		F = self.newton_residual(u_cur, u_old, t)
-		R = sum(F**2)
-		return R
-	def newton_solve(self, u0, u_old, t):
+	def solve(self, u_guess, u_old, t):
 		"""
 		Use newton's method to solve at time=1, using the initial guess u0
 		"""
-		if u0 is None:
-			u0 = np.zeros((self.N+1)*2)
-		res0 = self.newton_residual_norm(u0,u_old,t)
-		if res0 <= 1E-16:
-			return u0
+		if u_guess is None:
+			u_guess = np.zeros((self.N+1)*2)
 		ans = opt.leastsq(
 				func = self.newton_residual,
-				x0 = u0,
+				x0 = u_guess,
 				args = (u_old,t),
 				ftol=1E-15
-				#jac = self.newton_jacobian,
 				)
 		return ans[0]
 
@@ -396,7 +272,7 @@ def innerproduct(u, res, N, T):
 	return tau/2*sum(qoif_t[1::] + qoif_t[0:-1])
 def test_problem(R, sol, ph, th, T, ID):
 	u = FEM_SYS(ph.N, T, ph, th)
-	res = u.newton_solve(None,None,1)
+	res = u.solve(None,None,1)
 	qoia = innerproduct(res[0:ph.N+1], R, ph.N, T)
 	print("%8s: %8f %8f"%(ID,qoia, abs(qoia-sol)),end="\t")
 	print(res)
@@ -425,20 +301,3 @@ uth = "1-x"
 ph_1 = manufacture_solution(N, "1", "uph", uph, uth, True)
 th_1 = manufacture_solution(N, "1", "0", uph, uth, False)
 test_problem(R, 1/3, ph_1, th_1, T, "u="+uph)
-'''
-N = 2
-kph  = lambda x, t, ph_uq, th_uq: x
-Dkph = lambda x, t, ph_uq, th_uq: 0
-Qph  = lambda x, t, ph_uq, th_uq: 1
-BCph = (2,lambda t: 0,2,lambda t: 1)
-ph_x = diffusion_system(N, kph, Dkph, Qph, BCph, offset=0)
-test_problem(ph_x, T, 'u=x')
-
-N = 2
-kph  = lambda x, t, ph_uq, th_uq: 1
-Dkph = lambda x, t, ph_uq, th_uq: 0
-Qph  = lambda x, t, ph_uq, th_uq: 2*x
-BCph = (2,lambda t: 0,2,lambda t: 1)
-ph_x2 = diffusion_system(N, kph, Dkph, Qph, BCph, offset=0)
-test_problem(ph_x2, T, 'u=x²')
-'''
